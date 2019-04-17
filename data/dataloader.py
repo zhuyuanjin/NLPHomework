69fea94ee281318d46fc75f206adfe1b96d0d726
yangorwell <yangorwell@gmail.com>
2019-04-17 11:19:48 +0800

    fix bug

--- data/dataloader.py
+++ data/dataloader.py
@@ -25,35 +25,35 @@ def save_dataset(dataset, path):
         os.mkdir(path)
 
 
-def padding(data):
-    #data.sort(key=lambda x: len(x[0]), reverse=True)
-    query, response = zip(*data)
+# def padding(data):
+#     #data.sort(key=lambda x: len(x[0]), reverse=True)
+#     query, response = zip(*data)
 
-    # q_len = [len(s) for s in query]
+#     # q_len = [len(s) for s in query]
 
-    q_pad = torch.zeros(len(query),5, 50).long()
+#     q_pad = torch.zeros(len(query),5, 50).long()
 
-    for i, s in enumerate(query):
-        # end = q_len[i]
-        print(s)
-        s = torch.Tensor(s)
-        print(s.size())
-        # if len(s) > 5:
-            ## 保留后五个
+#     for i, s in enumerate(query):
+#         # end = q_len[i]
+#         print(s)
+#         s = torch.Tensor(s)
+#         print(s.size())
+#         # if len(s) > 5:
+#             ## 保留后五个
 
-        # q_pad[i, -1, -1] = torch.Tensor(s)
+#         # q_pad[i, -1, -1] = torch.Tensor(s)
 
-    r_len = [len(s) for s in response]
-    # print(max(r_len))
-    # print(query,q_pad)
-    r_pad = torch.zeros(len(response),1, 50).long()
-    for i, s in enumerate(response):
-        end = r_len[i]
-        r_pad[i,0, :end] = s[:end]
-    #tgt_len = [length-1 for length in tgt_len]
+#     r_len = [len(s) for s in response]
+#     # print(max(r_len))
+#     # print(query,q_pad)
+#     r_pad = torch.zeros(len(response),1, 50).long()
+#     for i, s in enumerate(response):
+#         end = r_len[i]
+#         r_pad[i,0, :end] = s[:end]
+#     #tgt_len = [length-1 for length in tgt_len]
 
-    #return src_pad.t(), src_len, tgt_pad.t(), tgt_len
-    return q_pad, r_pad
+#     #return src_pad.t(), src_len, tgt_pad.t(), tgt_len
+#     return q_pad, r_pad
 
 
 def get_loader(dataset, batch_size, shuffle, num_workers):
@@ -61,6 +61,6 @@ def get_loader(dataset, batch_size, shuffle, num_workers):
     data_loader = torch.utils.data.DataLoader(dataset=dataset,
                                               batch_size=batch_size,
                                               shuffle=shuffle,
-                                              num_workers=num_workers,
+                                              num_workers=num_workers
                                               )
     return data_loader
