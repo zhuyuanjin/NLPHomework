d2836b1eb9a7c95e280d579de3e5974edefd6a98
朱垣金 <35217055+zhuyuanjin@users.noreply.github.com>
2019-04-18 12:03:23 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -2,72 +2,46 @@ from preprocess import *
 from SCN import SCN_Net
 from torch import nn
 import torch
-import gensim 
 
-parser = argparse.ArgumentParser(description='train.py')
-
-
-parser.add_argument('-embedding_size', type = int, default=300)
-parser.add_argument('-save_data_dir', default='./save_data') 
-parser.add_argument('-is_save_data',default='True')
-parser.add_argument('-embedding_dir',default='./data/embedding/sgns.weibo.word')
-parser.add_argument('-q_file', default='./data/train/file_q.txt')
-parser.add_argument('-r_file', default='./data/train/file_r.txt')
-parser.add_argument('-neg_file', default='./data/train/neg_res.txt')
-
-opt = parser.parse_args()
 
 #file_path
 
 
 model_param = {
-    # 'embedding_matrix': torch.randn([200004, 300]),
+    'embedding_matrix': torch.randn([434511, 200]),
     'rnn_units': 200,
-    'word_embedding_size': opt.embedding_size
+    'word_embedding_size': 200,
+    'max_sentence_len':30
 }
 
 #The super paramters
-num_epoch = 10
+num_epoch = 1000
 lr = 1e-4
 batch_size = 4
-num_workers = 2
+num_workers = 0
 
-if opt.is_save_data is not None:
-	save_data = torch.load(opt.save_data_dir)
-	dicts = save_data['dicts']	
-
-	embedding_matrix = torch.randn(dicts.size(),opt.embedding_size)
-	# print(dicts.size())
-	gensim_model = gensim.models.KeyedVectors.load_word2vec_format(opt.embedding_dir)
-	for i in range(dicts.size()):
-		if dicts.idxToLabel[i] in gensim_model.wv.vocab:
-			embedding_matrix[i,:] = gensim_model[dicts.idxToLabel[i]]
-	trainset = save_data['train']
-else:
 
 # gett the dataoder from MingHan's Code
-	dicts = initVocabulary('source and target',
-	                                  opt.q_file,
-	                                  opt.vocab,
-	                                  opt.vocab_size)
-	trainset = makeData(opt.q_file, opt.r_file, dicts,opt.neg_file)
-	embedding_matrix = torch.randn(dicts.size(),opt.embedding_size)
-
+dicts = initVocabulary('source and target',
+                                  opt.q_file,
+                                  opt.vocab,
+                                  opt.vocab_size)
+trainset = makeData(opt.q_file, opt.r_file, dicts)
 
 trainloader = dataloader.get_loader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 
 #build the model
-model = SCN_Net(embedding_matrix,**model_param)
+model = SCN_Net(**model_param)
 opt = torch.optim.Adam(model.parameters(), lr=lr)
 
 for epoch in range(num_epoch):
     for batch in trainloader:
-        query, response , neg_response = batch
+        query, response = batch
 
         query = query.long()
         response = response.long()
-        logit = model(query, response,neg_response)
+        logit = model(query, response)
 
         y_true = torch.ones(batch_size).long()
         loss_fn = nn.CrossEntropyLoss()
@@ -76,3 +50,4 @@ for epoch in range(num_epoch):
         opt.zero_grad()
         loss.backward()
         opt.step()
+
